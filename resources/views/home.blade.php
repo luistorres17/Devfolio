583b939c7d345451234af9761883fb424d167f26
¨Ltorres¨ <luis_torres1798@icloud.com>
2025-04-06 19:14:55 -0600

    Alpha 2.0.1 bugs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
             <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 fixed top-0 left-0 right-0 z-50">
                 <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <a href="{{ route('home') }}" class="flex items-center">
-                        <img src="{{ asset('storage/icons/dev.svg') }}" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                        <img src="{{ asset('storage/icons/dev.svg') }}" class="mr-3 h-6 sm:h-9" alt="Logo Dev" />
                         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Mi Portafolio</span>
                     </a>
                     <div class="flex items-center lg:order-2">
